2a49ac1190a1ba41bcef764948a283627806792f
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-09-21 18:00:25 +0530

    first commit

---
+++ demo/chat/src/constants.ts
@@ -0,0 +1,4 @@
+export const SETTINGS: Map<string, string> = new Map([
+    ["CONVAI-API-KEY", "e6f6db881dd648f380cc51d6238297c8"],
+    ["CHARACTER-ID", "36720106-3283-11ee-8365-42010a40000b"]
+]);;
---
+++ demo/talking-avatar/src/constants.js
@@ -0,0 +1,4 @@
+export const SETTINGS = {
+    "CONVAI-API-KEY": "9113b63da7d4413124dfe52326adfdba",
+    "CHARACTER-ID": "b6a26244-e1ce-11ee-bcdd-42010a7be009"
+};
